e509cf6b0ca001d88d3d697d8d0e995e7522920b
Austin <Austin>
2016-10-08 13:09:54 -0400

    Added project

---
+++ HackNEHS/src/com/app/src/main/MainMenu.java
@@ -0,0 +1,15 @@
+package com.app.src.main;
+
+import javax.swing.JFrame;
+
+public class MainMenu
+{
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame("CLOTHING APP");
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+	}
+}
